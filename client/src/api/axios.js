9ad72b91552e286e66682102dd0e9fd1fb9b517c
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-07-17 13:44:55 -0700

    update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: 'https://task-management-i6yl.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
